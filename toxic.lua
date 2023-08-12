d6ad8533b6d2e19a9f1741db672ed1911d069d7f
BigJoeyJoeyFunnies <140270124+BigJoeyJoeyFunnies@users.noreply.github.com>
2023-08-11 17:29:44 -0700

    Update toxic.lua

--- toxic.lua
+++ toxic.lua
@@ -1,31 +1,25 @@
-local function onPlayerDied(victim, killer)
-    if killer and killer:IsA("Player") then
-        local victimName = victim.Name
-        local killerName = killer.Name
-        
-        local killMessage = "Killed " .. victimName .. " - Rest in peace!"
-        local customMessage = "L " .. victimName .. " - Gone but not forgotten!"
-        local customMessage2 = "Lost " .. victimName .. " - skillissue"
+local function onPlayerDied(victim)
+    local playerName = game.Players.LocalPlayer.Name
+    
+    local killMessage = "Killed " .. victim .. " - Rest in peace!"
+    local customMessage = "L " .. victim .. " - Gone but not forgotten!"
+    local customMessage2 = "Lost " .. victim .. " - skillissue"
 
-        
-        -- Say messages in chat
-        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(
-            killMessage, "All"
-        )
-        
-        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(
-            customMessage, "All"
-        )
+    -- Say messages in chat
+    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(
+        killMessage, "All"
+    )
+    
+    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(
+        customMessage, "All"
+    )
   
-        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(
-            customMessage2, "All"
-        )
-    end
+    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(
+        customMessage2, "All"
+    )
 end
 
-game.Players.PlayerAdded:Connect(function(player)
-    player.Character:WaitForChild("Humanoid").Died:Connect(function()
-        onPlayerDied(player, player:FindFirstChild("creator"))
-    end)
+local player = game.Players.LocalPlayer
+player.Character:WaitForChild("Humanoid").Died:Connect(function()
+    onPlayerDied(player.Name)
 end)
- 
